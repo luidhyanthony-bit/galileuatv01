22f67ad6e28281ed61e3e79dfc823e8547052a4c
luidhyIFRN <luidhy.anthony@academico.ifrn.edu.br>
2026-06-10 20:31:46 -0300

    new

--- galileu/x.py
+++ galileu/x.py
@@ -1,2 +1,22 @@
 import socket
-HOST = 
+
+host = "viacep.com.br"
+porta = 80
+request = f"GET / HTTP/1.1\r\nHost: {host}\r\n\r\n"
+
+falar(f"Conectando em {host}...")
+try:
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cliente.connect((host, porta))
+    
+    falar("send HTTP...")
+    cliente.sendall(request.encode('utf-8'))
+    
+    resposta = b""
+    while True:
+        dados = cliente.recv(4096)
+        if not dados: break
+        resposta += dados
+    print(resposta)
+    cliente.close()
+
